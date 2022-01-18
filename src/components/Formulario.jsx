9363532aa8f587bd70af822c809071930a3b9229
Andre Levi Neri Longo <andre.levi@futuraonline.com.br>
2022-01-18 14:57:12 -0300

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,19 @@
+import { Component } from "react/cjs/react.production.min"
+import React from "react"
+
+class Formulario extends Component{
+
+    render() {
+        return (
+        <section>
+            <form>
+              <input type="text" placeholder="Titulo"/>
+              <textarea placeholder="Escreva sua nota..."/>
+              <button>Criar Nota</button>
+            </form>
+        </section>
+        )
+    }
+}
+
+export default Formulario;
